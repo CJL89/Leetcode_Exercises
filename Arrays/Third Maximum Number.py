5d1b36ecc8f30f8ea12eb75f7ec609245d6e55f6
Cesar Luis Jaitman Labaton <CJL89@users.noreply.github.com>
2020-11-18 20:36:50 -0500

    Finished problem

--- Arrays/Third Maximum Number.py
+++ Arrays/Third Maximum Number.py
@@ -1,14 +1,43 @@
 nums = [2, 2, 3, 1]
+nums2 = [1, 2]
+nums3 = [1]
+nums4 = [1, 1, 2]
+nums5 = [1,2,2,5,3,5]
 
 def thirdMax(nums):
+    # Making sure that array is filled
     if nums == 0:
         return nums
 
-    pos = 0
+    # If array has less than 3 elements return the max
+    if len(nums) < 3:
+        return max(nums)
 
-    for i in range(len(nums)):
-        print(i)
+    pos = []
 
+    if len(nums) >= 3:
+        # nums = sorted(nums)
+        for i in range(len(nums)):
+            if nums[i] not in pos:
+                pos.append(nums[i])
 
+        if len(pos) < 3:
+            return max(pos)
 
-print(thirdMax(nums))
+        return pos[2]
+
+print(thirdMax(nums5))
+
+# Not correct output
+def thirdMax(nums):
+    s = set(nums)
+    print(s)
+
+    if len(s) < 3:
+        return max(s)
+
+    s.remove(max(s))
+    s.remove(max(s))
+    return(max(s))
+
+print(thirdMax(nums5))
